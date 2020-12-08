913e6d17db748b3254a76555dd3918cd30adc5a0
Ilya Omelyukhin <i.omelyuhin@hmail.com>
2020-12-08 11:28:02 +0300

    initial

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+
+from views import index_app
+from views import catalog_app
+from views import contact_app
+
+app = Flask(__name__)
+app.register_blueprint(index_app)
+app.register_blueprint(catalog_app)
+app.register_blueprint(contact_app)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    name = "World"
+    if request.method == "POST":
+        name = request.form.get('name', 'World')
+    return render_template("index.html", name=name)
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    return render_template("contact.html")
+
+
+# @app.route("/catalog", methods=["GET", "POST"])
+# def catalog():
+#     return f"<h1>Catalog</h1>"
---
+++ views/catalog.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, render_template, request, jsonify
+from werkzeug.exceptions import BadRequest
+
+catalog_app = Blueprint("catalog_app", __name__)
+
+
+def get_default_products():
+    return {
+        1: "Развивающие книги",
+        2: "Конструкторы",
+        3: "Раскраски"
+    }
+
+
+PRODUCTS = get_default_products()
+
+
+@catalog_app.route("/catalog")
+def catalog_list():
+    return render_template("catalog/index.html", products=PRODUCTS)
+
+
+@catalog_app.route("/<int:product_id>/", methods=['GET', 'DELETE'])
+def product_detail(product_id: int):
+    try:
+        product_name = PRODUCTS[product_id]
+    except KeyError:
+        raise BadRequest(f"Invalid product id #{product_id}")
+
+    if request.method == 'DELETE':
+        PRODUCTS.pop(product_id)
+        return jsonify(ok=True)
+
+    return render_template(
+        "catalog/detail.html",
+        product_id=product_id,
+        product_name=product_name
+    )
+
+
+@catalog_app.route("/recover/", methods=["POST"])
+def recover_products():
+    PRODUCTS.update(get_default_products())
+    return jsonify(ok=True)
